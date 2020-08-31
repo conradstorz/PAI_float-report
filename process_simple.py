2bb2ea506ab8ca836a6c7d51697b4dda84fcbc50
Conrad Storz <conradstorz@gmail.com>
2020-08-31 08:32:26 -0400

    improved handling of drop columns

--- process_simple.py
+++ process_simple.py
@@ -9,6 +9,7 @@ of the spreadsheet in dataframe format.
 import csv
 from loguru import logger
 import pandas as panda
+import json
 from customize_dataframe_for_excel import set_custom_excel_formatting
 
 
@@ -17,6 +18,11 @@ def process_simple_summary_csv(out_f, in_f, rundate):
     """Scan file and compute sums for 2 columns
     """
     df = panda.read_csv(in_f)
+
+    FORMATTING_FILE = 'ColumnFormatting.json'
+    with open(FORMATTING_FILE) as json_data:
+        column_details = json.load(json_data)
+    # this dictionary will contain information about individual terminals
 
     DAYS = 30
 
@@ -51,7 +57,7 @@ def process_simple_summary_csv(out_f, in_f, rundate):
             return 0
 
     try:
-        df['Surcharge WDs'] = df.apply(lambda row: calc(row), axis=1)
+        df['Surcharge amt'] = df.apply(lambda row: calc(row), axis=1)
     except KeyError as e:
         logger.error(f'KeyError in dataframe: {e}')
         return False
@@ -83,7 +89,10 @@ def process_simple_summary_csv(out_f, in_f, rundate):
         return False
 
     # work is finished. Drop unneeded columns from output
-    df = df.drop(df.columns[[0, 2, 3, 4, 7]], axis=1)  # df.columns is zero-based panda.Index
+    # TODO expand this to drop all columns except those desired in the report
+    df = df.drop([
+                'Settlement Date'
+                ], axis=1)  # df.columns is zero-based panda.Index
 
     # sort the data
     df = df.sort_values('Surch',ascending=False)
@@ -91,9 +100,7 @@ def process_simple_summary_csv(out_f, in_f, rundate):
     # define column formats
     writer = panda.ExcelWriter(out_f, engine='xlsxwriter')
     df.to_excel(writer, startrow = 1, sheet_name='Sheet1', index=False)    
-    a,n,c,p = 'A#$%'
-    formats = [a,c,c,c,c]
-    set_custom_excel_formatting(df, writer, formats)
+    set_custom_excel_formatting(df, writer, column_details)
 
     logger.info('All work done. Saving worksheet...') 
     writer.save()    
