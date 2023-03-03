78f2962444ed89db138e2ce123d6cf5f05098d63
Conrad Storz <conradstorz@gmail.com>
2023-03-03 11:03:13 -0500

    fixed printing problem

--- CSV_munge/reportlabfunction.py
+++ CSV_munge/reportlabfunction.py
@@ -1,5 +1,7 @@
 """function to establish standard style for dataframe output"""
 
+from datetime import datetime
+from time import sleep
 # import win32api for printing of PDFs
 import win32api
 # import the report building functions
@@ -7,13 +9,16 @@ from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter, landscape
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
 
-def Send_to_printer(df):
+def Send_to_printer(Dataframe_to_be_printed):
     """Uses ReportLab module to build a PDF and then send to the default printer."""
+    # Get the current date to use in the file name
+    today = datetime.now().strftime('%Y-%m-%d-%H%M%S')
+    pdffile = f'temporary_{today}_pdf_data.pdf'
     # Create a PDF document
-    doc = SimpleDocTemplate("temporary_print.pdf", pagesize=landscape(letter))
+    doc = SimpleDocTemplate(pdffile, pagesize=landscape(letter))
 
     # Create a table with the contents of the dataframe
-    table = Table([df.columns.tolist()] + df.values.tolist())
+    table = Table([Dataframe_to_be_printed.columns.tolist()] + Dataframe_to_be_printed.values.tolist())
 
     # Apply table style
     table.setStyle(TableStyle([
@@ -30,5 +35,8 @@ def Send_to_printer(df):
     doc.build([table])
 
     # Print the PDF document
-    win32api.ShellExecute(0, "print", "temporary_print.pdf", None, ".", 0)
+    status = win32api.ShellExecute(0, "print", pdffile, None, ".", 0)
+    print(status)
+    print('Pausing 30 seconds to allow PDF to print...')
+    sleep(30) # provide time for output PDF to be processed
     return
