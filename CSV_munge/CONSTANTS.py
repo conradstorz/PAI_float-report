c612eae0ea0ee401fceb9318003ae9ad7e49dddc
Conrad Storz <conradstorz@gmail.com>
2023-03-03 11:03:39 -0500

    work progress - routine

--- CSV_munge/CONSTANTS.py
+++ CSV_munge/CONSTANTS.py
@@ -1,6 +1,6 @@
 # declare the details for this script
-PaymentAllianceFilename = 'database2.csv'
-ColumbusDataFilename = 'database1.csv'
+PaymentAllianceFilename = 'PAI_2023_jan.csv'
+ColumbusDataFilename = 'CDS_2023_jan.csv'
 Terminal_Details = "Terminal_Details.json"
-REPORTING_PERIOD_DAYS = 30 # days
-ANNUAL_BORROWING_RATE = 0.04  # percentage rate paid for borrowing cash to fund ATMs
+REPORTING_PERIOD_DAYS = 31 # days
+ANNUAL_BORROWING_RATE = 0.12  # percentage rate paid for borrowing cash to fund ATMs
